2ff6bfc6698ec8ccd351b6201a6f9fc5c8609881
hadley <h.wickham@gmail.com>
2014-01-22 08:28:56 -0600

    Improve melting example

--- data/melt.r
+++ data/melt.r
@@ -1,7 +1,7 @@
 library(reshape2)
 source("xtable.r")
 
-df <- data.frame(row = letters[1:3], a = 1:3, b = 4:6, c = 7:9)
+df <- data.frame(row = LETTERS[1:3], a = 1:3, b = 4:6, c = 7:9)
 xtable(df, "melt-raw.tex")
 
 dfm <- melt(df, id = "row")
